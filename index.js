17b09359066294d53c0b56436cd66c992e1f8a95
David Ellis <d.f.ellis@ieee.org>
2014-03-20 14:29:43 -0700

    Pull out CacheDir for open sourcing. Debating whether or not 'mutate' should become a separate library

--- index.js
+++ index.js
@@ -1,5 +1,89 @@
-function cacheDir() {
-    
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp').sync;
+var async = require('async');
+
+function CacheDir(config) {
+    this.dir = config.dir;
+    this.namespace = config.namespace;
+    this.lazy = config.lazy || false;
+    this.cache = {};
+
+    var namespacedDir = path.join(this.dir, this.namespace);
+    if (!fs.existsSync(namespacedDir)) {
+        mkdirp(namespacedDir);
+    } else if (!this.lazy) {
+        fs.readdirSync(namespacedDir).forEach(function(file) {
+            this.getSync(file);
+        }.bind(this));
+    }
 }
 
-module.exports = cacheDir;
+CacheDir.prototype.setSync = function(key, val) {
+    this.cache[key] = val;
+    fs.writeFileSync(path.join(this.dir, this.namespace, key), JSON.stringify(val));
+};
+
+CacheDir.prototype.set = function(key, val, callback) {
+    this.cache[key] = val;
+    fs.writeFile(path.join(this.dir, this.namespace, key), JSON.stringify(val), callback);
+};
+
+CacheDir.prototype.getSync = function(key) {
+    if (this.cache.hasOwnProperty(key)) return this.cache[key];
+    var source = path.join(this.dir, this.namespace, key);
+    if (fs.existsSync(source)) {
+        try {
+            this.cache[key] = JSON.parse(fs.readFileSync(source, 'utf8'));
+        } catch (e) {
+            fs.unlinkSync(source);
+        }
+    }
+    return this.cache[key];
+};
+
+CacheDir.prototype.get = function(key, callback) {
+    if (this.cache.hasOwnProperty(key)) return process.nextTick(callback.bind(this, null, this.cache[key]));
+    var source = path.join(this.dir, this.namespace, key);
+    fs.exists(source, function(exists) {
+        if (!exists) return callback();
+        fs.readFile(source, 'utf8', function(err, result) {
+            // Errors with the cache file are ignored, treated as if nothing is cached
+            if (err) return callback(err);
+            try {
+                this.cache[key] = JSON.parse(result);
+            } catch (e) {
+                return fs.unlink(source, callback);
+            }
+            callback(null, this.cache[key]);
+        }.bind(this));
+    }.bind(this));
+};
+
+CacheDir.prototype.keysSync = function() {
+    return Object.keys(this.cache);
+};
+
+CacheDir.prototype.keys = function(callback) {
+    fs.readdir(path.join(this.dir, this.namespace), callback);
+};
+
+CacheDir.prototype.valuesSync = function() {
+    return Object.keys(this.cache).map(function(key) { return this.cache[key]; }.bind(this));
+};
+
+CacheDir.prototype.values = function(callback) {
+    fs.readdir(path.join(this.dir, this.namespace), function(err, keys) {
+        if (err) return callback(err);
+        async.map(keys, function(key, next) {
+            fs.readFile(path.join(this.dir, this.namespace, key), 'utf8', function(err, val) {
+                if (err) return next(err);
+                next(null, JSON.parse(val));
+            });
+        }.bind(this), callback);
+    }.bind(this));
+};
+
+// TODO: Add other ES6 Map methods: items, has, forEach, iterator, delete, clear, toString, and the property size
+
+module.exports = CacheDir;
--- test/index.js
+++ test/index.js
@@ -1,8 +1,207 @@
+var CacheDir = require('..');
+var fs = require('fs');
+var mutate = require('../lib/mutate');
+var path = require('path');
+var exec = require('child_process').exec;
 var test = require('tape');
 
-var cacheDir = require('../index.js');
+test('clear', function (assert) {
+    exec('rm -rf ' + path.join(__dirname, 'cacheTest'), function() {
+        assert.end();
+    });
+});
 
-test('cacheDir is a function', function (assert) {
-    assert.strictEqual(typeof cacheDir, 'function');
+function createCache() {
+    return new CacheDir({
+        dir: __dirname,
+        namespace: 'cacheTest'
+    });
+}
+
+function makePath(key) {
+    return key ? path.join(__dirname, 'cacheTest', key) : path.join(__dirname, 'cacheTest');
+}
+
+test('initialize', function (assert) {
+    assert.plan(3);
+    assert.ok(!fs.existsSync(makePath()), 'cache directory does not yet exist');
+    var cache = createCache();
+    assert.ok(fs.existsSync(makePath()), 'auto-created the directory');
+    assert.equal(cache.lazy, false, 'greedy by default');
     assert.end();
+});
+
+test('setSync', function (assert) {
+    assert.plan(2);
+    var cache = createCache();
+    cache.setSync('foo', 'bar');
+    assert.ok(fs.existsSync(makePath('foo')), 'key written to disk');
+    assert.equal(JSON.parse(fs.readFileSync(makePath('foo'), 'utf8')), cache.cache.foo, 'written value matches memory');
+    assert.end();
+});
+
+test('set', function (assert) {
+    assert.plan(2);
+    var cache = createCache();
+    cache.set('foo2', 'bar2', function() {
+        assert.ok(fs.existsSync(makePath('foo2')), 'key written to disk');
+        assert.equal(JSON.parse(fs.readFileSync(makePath('foo2'), 'utf8')), cache.cache.foo2, 'written value matches memory');
+        assert.end();
+    });
+});
+    
+
+test('getSync', function (assert) {
+    assert.plan(1);
+    var cache = createCache();
+    var val = cache.getSync('foo');
+    assert.equal(val, 'bar', 'got the expected value even from a separate constructor obj');
+    assert.end();
+});
+
+test('get sync non existent', function (assert) {
+    assert.plan(1);
+    var cache = createCache();
+    var val = cache.getSync('nonexistent');
+    assert.equal(val, undefined, 'got the expected undefined value for a nonexistent key');
+    assert.end();
+});
+
+test('get sync garbled file', function (assert) {
+    assert.plan(2);
+    var cache = createCache();
+    fs.writeFileSync(path.join(__dirname, 'cacheTest', 'garbled'), 'lol{json}');
+    var val = cache.getSync('garbled');
+    assert.equal(val, undefined, 'garbled data is also returned as undefined');
+    assert.ok(!fs.existsSync(path.join(__dirname, 'cacheTest', 'garbled')), 'garbled was deleted');
+    assert.end();
+});
+
+test('get', function (assert) {
+    assert.plan(2);
+    var cache = createCache();
+    cache.get('foo2', function(err, val) {
+        assert.ifError(err, 'should not get an error back');
+        assert.equal(val, 'bar2', 'got the expected value from a separate constructor obj');
+        assert.end();
+    });
+});
+
+test('getLazy', function (assert) {
+    assert.plan(3);
+    var cache = new CacheDir({
+        dir: __dirname,
+        namespace: 'cacheTest',
+        lazy: true
+    });
+    assert.ok(!cache.cache.foo2, 'key not yet in memory');
+    cache.get('foo2', function(err, val) {
+        assert.ifError(err, 'should not get an error back');
+        assert.equal(val, 'bar2', 'got the expected value even though not originally in memory');
+        assert.end();
+    });
+});
+
+test('get non existent', function (assert) {
+    assert.plan(2);
+    var cache = createCache();
+    cache.get('nonexistent', function(err, result) {
+        assert.ifError(err, 'got no error');
+        assert.equal(result, undefined, 'got no result');
+        assert.end();
+    });
+});
+
+test('get garbled', function (assert) {
+    assert.plan(3);
+    var cache = createCache();
+    fs.writeFileSync(path.join(__dirname, 'cacheTest', 'garbled'), 'lol{json}');
+    cache.get('garbled', function(err, result) {
+        assert.ifError(err, 'got no error');
+        assert.equal(result, undefined, 'got no data for the garbled file');
+        assert.ok(!fs.existsSync(path.join(__dirname, 'cacheTest', 'garbled')), 'garbled was deleted');
+        assert.end();
+    });
+});
+
+test('get fs failure', mutate(fs, 'readFile', function (source, mode, callback) {
+    callback(new Error('fake error'));
+}, function (assert) {
+    assert.plan(2);
+    var cache = new CacheDir({
+        dir: __dirname,
+        namespace: 'cacheTest',
+        lazy: true
+    });
+    cache.get('foo2', function(err) {
+        assert.ok(err instanceof Error, 'got an error back');
+        assert.equal(err.message, 'fake error', 'got the fake fs.readFile error back');
+        assert.end();
+    });
+}));
+
+test('keys sync', function (assert) {
+    assert.plan(1);
+    var cache = createCache();
+    assert.deepEqual(cache.keysSync().sort(), ['foo', 'foo2'], 'got both keys');
+    assert.end();
+});
+
+test('keys', function (assert) {
+    assert.plan(1);
+    var cache = createCache();
+    cache.keys(function(err, keys) {
+        assert.deepEqual(keys.sort(), ['foo', 'foo2'], 'got both keys');
+        assert.end();
+    });
+});
+
+test('valuesSync', function (assert) {
+    assert.plan(1);
+    var cache = createCache();
+    assert.deepEqual(cache.valuesSync().sort(), ['bar', 'bar2'], 'got both values');
+    assert.end();
+});
+
+test('values', function (assert) {
+    assert.plan(1);
+    var cache = createCache();
+    cache.values(function(err, values) {
+        assert.deepEqual(values.sort(), ['bar', 'bar2'], 'got both values');
+        assert.end();
+    });
+});
+
+test('valuesReaddirFailure', mutate(fs, 'readdir', function(path, callback) {
+    callback(new Error('lol readdir failed somehow'));
+}, function(assert) {
+    assert.plan(2);
+    var cache = createCache();
+    cache.values(function(err) {
+        assert.ok(err instanceof Error, 'got an error back');
+        assert.equal(err.message, 'lol readdir failed somehow', 'got my fake readdir error back');
+        assert.end();
+    });
+}));
+
+test('valuesReadFileFailure', mutate(fs, 'readFile', function(path, mode, callback) {
+    callback(new Error('lol readFile also failed'));
+}, function(assert) {
+    assert.plan(2);
+    var cache = new CacheDir({
+        dir: __dirname,
+        namespace: 'cacheTest',
+        lazy: true
+    });
+    cache.values(function(err) {
+        assert.ok(err instanceof Error, 'got an error back');
+        assert.equal(err.message, 'lol readFile also failed', 'got my fake readFile error back');
+        assert.end();
+    });
+}));
+
+test('clearAgain', function (assert) {
+    exec('rm -rf ' + path.join(__dirname, 'cacheTest'), function() {
+        assert.end();
+    });
 });
